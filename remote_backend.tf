0fbfc095e3f1abc2bbd277e8d41e0beaf374791f
jamorrison <jamorrison@costco.com>
2019-09-04 12:47:18 -0700

    changes

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "hashicorp-workshop"
+    organization = "jamorrison-costco-sandbox"
     workspaces {
-      name = "YOURWORKSPACE"
+      name = "hashicat-azure"
     }
   }
 }
